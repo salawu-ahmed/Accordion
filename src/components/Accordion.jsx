e68374f2c8e463725f1f0f72fccdc40c9884b803
ahmed <a.salawu@outlook.com>
2024-02-27 01:49:58 +0000

    Update: Accordion opens on click (single selection)

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import  {data}  from '../data/data.js'
+// single selection
+// multiple selection
+
+const Accordion = () => {
+    const [selected, setSelected] = useState(null)
+    const handleSingleSelection = (id) => {
+      console.log(id);
+      setSelected(id)
+    }
+  return (
+    <div className='wrapper'>
+      <div className="accordion">
+        {
+            data && data.length > 0 ? (
+                data.map((item) => (
+                    <div className="item" key={item.id}>
+                        <div className="title" onClick={() => handleSingleSelection(item.id)}>
+                            <h3>{item.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                          selected === item.id ? (
+                            <div className="content">{item.answer}</div>
+                          ) : (
+                            null
+                          )
+                        }
+                    </div>
+                ))
+            ) : (
+                <div>Error message</div>
+            )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Accordion
---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        id: 1,
+        question: "How does react virtual DOM work?",
+        answer: "The react virtual DOM is a ......."
+    },
+    {
+        id: 2,
+        question: "How does react virtual DOM work?",
+        answer: "The react virtual DOM is a ......."
+    },
+    {
+        id: 3,
+        question: "How does react virtual DOM work?",
+        answer: "The react virtual DOM is a ......."
+    },
+    {
+        id: 4,
+        question: "How does react virtual DOM work?",
+        answer: "The react virtual DOM is a ......."
+    },
+    {
+        id: 5,
+        question: "How does react virtual DOM work?",
+        answer: "The react virtual DOM is a ......."
+    },
+]
