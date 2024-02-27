b21c660a13b059ca927f6d6c0a21fa6e0b100386
ahmed <a.salawu@outlook.com>
2024-02-27 02:08:43 +0000

    Update: Created Single Selection Accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import  {data}  from '../data/data.js'
+import styles from './Accordion.module.css'
 // single selection
 // multiple selection
 
@@ -10,19 +11,19 @@ const Accordion = () => {
       setSelected(id === selected ? null : id)
     }
   return (
-    <div className='wrapper'>
-      <div className="accordion">
+    <div className={styles.wrapper}>
+      <div className={styles.accordion}>
         {
             data && data.length > 0 ? (
                 data.map((item) => (
-                    <div className="item" key={item.id}>
-                        <div className="title" onClick={() => handleSingleSelection(item.id)}>
+                    <div className={styles.item} key={item.id}>
+                        <div className={styles.title} onClick={() => handleSingleSelection(item.id)}>
                             <h3>{item.question}</h3>
                             <span>+</span>
                         </div>
                         {
                           selected === item.id ? (
-                            <div className="content">{item.answer}</div>
+                            <div className={styles.content}>{item.answer}</div>
                           ) : (
                             null
                           )
